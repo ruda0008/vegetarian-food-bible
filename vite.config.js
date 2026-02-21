9d6dd0dcdc5a8d9f28a70ae4ddfe280a0605c9fb
ruda0008 <ruda0008@algonquinlive.com>
2026-02-20 22:33:39 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nutrition-guide/',
+  base: '/vegetarian-food-bible/',
   plugins: [react()],
 })
